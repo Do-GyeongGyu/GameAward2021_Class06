2a60c47a531f61cd35ac70ab36bf859bb2755a37
kamesanbrother <55975181+kamesanbrother@users.noreply.github.com>
2021-05-05 15:19:43 +0900

    タイマー作ったよ

---
+++ Assets/02_Game/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    private float countTime;//時間を格納
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countTime -= Time.deltaTime;//経過時間を引いてる
+
+        if (countTime < 0) countTime = 0;//0未満で0を格納
+
+        GetComponent<Text>().text = countTime.ToString("F2");//テキストに表示　F2は小数点第2まで
+      
+    }
+}
